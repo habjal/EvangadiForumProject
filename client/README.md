e8dad513d5dda594986e4f0f6ffe89f913c1a7b7
Fantahun (lej finto) <fantahun731@gmail.com>
2024-09-30 05:48:09 +0300

    project starter files

---
+++ client/README.md
@@ -0,0 +1,6 @@
+# EVANGADI FORUM PROJECT - EVANGADI BOOTCAMP
+
+## project collaboration links
+  - Trello   https://trello.com/b/OwH0iT7Y/evangadi-forum-g3
+  - Github Repo https://github.com/Fantahun/EvangadiForumProject
+  
---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>evangadi forum starter project</h1>
+    </>
+  )
+}
+
+export default App
