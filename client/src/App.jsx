7bc793acdd6631710ce50e3de61e5d88baaaadd7
Mohammed Ali <mohammedaliwedaj@gmail.com>
2024-10-04 05:18:14 +0300

    AskQuestion page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import Question from './Pages/Question/Question'
+// import Question from './Pages/Question/Question'
+import AskQuestion from './Pages/Question/AskQuestion/AskQuestion'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Question/>
+    <AskQuestion/>
+ 
      
     </>
   )
