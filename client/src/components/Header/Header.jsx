2fd560905d77a46d66f50ac35029fc320e483e6c
Hellen-Shiferaw <yilmahellen@gmail.com>
2024-10-03 22:30:20 -0400

    header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import styles from "./header.module.css";
+import React from "react";
+import classes from "./header.module.css";
+import EvangadiLogo from "../../Assets/Images/evangadi-logo-header.png";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <>
+      <section className={classes.outer_container}>
+        <div className={classes.header_container}>
+          <div className={classes.left_container}>
+            <div className={classes.logo_container}>
+              <img src={EvangadiLogo} alt="Evangadi Logo" />
+            </div>
+          </div>
+          <div className={classes.right_container}>
+            <nav className={classes.nav_links}>
+              <a href="#">Home</a>
+              <a href="#">How it Works</a>
+            </nav>
+            <button className={classes.logout_btn}>Log Out</button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Header
+export default Header;
