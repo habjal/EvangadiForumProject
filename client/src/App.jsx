1add66adad0363c52cb52b2353e73f1f8f14150b
Tsegaye <tsegayeayele78@gmail.com>
2024-10-04 18:55:24 +0300

    About

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 // import Header from "./components/Header/Header";
-
 // import Answer from "./pages/Answer/Answer";
 import About from "./Pages/About/About";
 
 function App() {
   return (
     <>
-      <About/>
+      <About />
     </>
   );
-
 }
-
 export default App;
--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from '../About/about.module.css';  
+import styles from '/about.module.css';  
 
 const About = () => {
   return (
