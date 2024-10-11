b97ee211f69ea5159d0c34fca92b5e84bf9379a9
Fantahun <Fantahun731@gmail.com>
2024-10-11 18:12:23 +0300

    removed .jsx on router for vercel issue

--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HowItWorks from "../Pages/Howitworks/HowItWorks";
+import { Routes, Route } from "react-router-dom";
 import Terms from "../components/Footer/Terms";
 import PrivacyPolicy from "../Pages/privacy-policy/Privacy-police";
 import QuestionAndAnswer from "../Pages/QuestionAndAnswer/QuestionAndAnswer";
 import AuthLayout from "../Pages/AuthLayout/AuthLayout";
 import PageNotFound from "../Pages/PageNotFound/PageNotFound";
 import ForgotPassword from "../Pages/ForgotPassword/ForgotPassword";
-import AskQuestion from "../Pages/Question/AskQuestion/AskQuestion";
 import Home from '../Pages/Home/Home'
+import Howitworks from '../Pages/Howitworks/HowItWorks'
+import AskQuestion from "../pages/Question/AskQuestion/AskQuestion";
+
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/ask" element={<AskQuestion />} />
       <Route path="/question/:questionId" element={<QuestionAndAnswer />} />
-      <Route path="/howitworks" element={<HowItWorks />} />
+      <Route path="/howitworks" element={<Howitworks />} />
       <Route path="/terms" element={<Terms />} />
       <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
       <Route path="/auth" element={<AuthLayout />} />
