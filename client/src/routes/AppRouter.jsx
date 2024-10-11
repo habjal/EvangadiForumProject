0ba52bee5f77283aabc75ec79577b88fd62b2b31
Fantahun <Fantahun731@gmail.com>
2024-10-11 18:07:52 +0300

    removed .jsx on router for vercel issue

--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../Pages/Home/Home";
 import HowItWorks from "../Pages/Howitworks/HowItWorks";
 import Terms from "../components/Footer/Terms";
 import PrivacyPolicy from "../Pages/privacy-policy/Privacy-police";
@@ -9,6 +8,7 @@ import AuthLayout from "../Pages/AuthLayout/AuthLayout";
 import PageNotFound from "../Pages/PageNotFound/PageNotFound";
 import ForgotPassword from "../Pages/ForgotPassword/ForgotPassword";
 import AskQuestion from "../Pages/Question/AskQuestion/AskQuestion";
+import Home from '../Pages/Home/Home'
 function AppRouter() {
   return (
     <Routes>
