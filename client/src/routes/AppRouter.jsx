cf4b2136de8ab46d2d7b6eb5ccc2110be44c9dcd
Fantahun <Fantahun731@gmail.com>
2024-10-11 18:00:07 +0300

    removed .jsx on router for vercel issue

--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../Pages/Home/Home.jsx";
-import HowItWorks from "../Pages/Howitworks/HowItWorks.jsx";
-import Terms from "../components/Footer/Terms.jsx";
-import PrivacyPolicy from "../Pages/privacy-policy/Privacy-police.jsx";
-import QuestionAndAnswer from "../Pages/QuestionAndAnswer/QuestionAndAnswer.jsx";
-import AuthLayout from "../Pages/AuthLayout/AuthLayout.jsx";
-import PageNotFound from "../Pages/PageNotFound/PageNotFound.jsx";
-import ForgotPassword from "../Pages/ForgotPassword/ForgotPassword.jsx";
-import AskQuestion from "../Pages/Question/AskQuestion/AskQuestion.jsx";
+import Home from "../Pages/Home/Home";
+import HowItWorks from "../Pages/Howitworks/HowItWorks";
+import Terms from "../components/Footer/Terms";
+import PrivacyPolicy from "../Pages/privacy-policy/Privacy-police";
+import QuestionAndAnswer from "../Pages/QuestionAndAnswer/QuestionAndAnswer";
+import AuthLayout from "../Pages/AuthLayout/AuthLayout";
+import PageNotFound from "../Pages/PageNotFound/PageNotFound";
+import ForgotPassword from "../Pages/ForgotPassword/ForgotPassword";
+import AskQuestion from "../Pages/Question/AskQuestion/AskQuestion";
 function AppRouter() {
   return (
     <Routes>
