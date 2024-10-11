a22742ec93abd80f9c3028774d72c9d13cb45a5a
Fantahun <Fantahun731@gmail.com>
2024-10-11 15:27:22 +0300

    build fix for production error on vercel

--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -1,33 +1,30 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
-import Home from '../pages/Home/Home'
-import AskQuestion from "../pages/Question/AskQuestion/AskQuestion";
+import Home from "../pages/Home/Home";
+import AskQuestion from "../pages/AskQuestion/AskQuestion";
 import HowItWorks from "../pages/Howitworks/HowItWorks";
-import Terms from '../components/Footer/Terms'
-import Privacy from '../components/Footer/Privacy'
-import PrivacyPolicy from '../pages/privacy-policy/Privacy-police'
+import Terms from "../components/Footer/Terms";
+import Privacy from "../components/Footer/Privacy";
+import PrivacyPolicy from "../pages/privacy-policy/Privacy-police";
 import QuestionAndAnswer from "../pages/QuestionAndAnswer/QuestionAndAnswer";
 import Answer from "../pages/Answer/Answer";
 import AuthLayout from "../pages/AuthLayout/AuthLayout";
 import PageNotFound from "../pages/PageNotFound/PageNotFound";
 import ForgotPassword from "../pages/ForgotPassword/ForgotPassword";
 function AppRouter() {
-    return (
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/ask" element={<AskQuestion/>}/>
-                <Route path="/question/:questionId" element={<QuestionAndAnswer/>}/>
-                <Route path='/howitworks' element={<HowItWorks/>}/>
-                <Route path="/terms" element={<Terms/>}/>
-                <Route path="/PrivacyPolicy" element={<PrivacyPolicy/>} />
-                <Route path="/auth" element={<AuthLayout/>} />
-                <Route path="/forgetPass" element={<ForgotPassword/>} />
-                <Route path="*" element={<PageNotFound/>} />
-
-
-            </Routes>
-    
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/ask" element={<AskQuestion />} />
+      <Route path="/question/:questionId" element={<QuestionAndAnswer />} />
+      <Route path="/howitworks" element={<HowItWorks />} />
+      <Route path="/terms" element={<Terms />} />
+      <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+      <Route path="/auth" element={<AuthLayout />} />
+      <Route path="/forgetPass" element={<ForgotPassword />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
   );
 }
 
